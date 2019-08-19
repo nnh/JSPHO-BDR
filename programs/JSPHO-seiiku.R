9e227de86ef97ea0c20412cd1d1e58197a1fcfc4
ma3k0 <mamiko.yonejima@nnh.go.jp>
2019-08-19 15:33:12 +0900

    成育提出用データセット作成

--- programs/JSPHO-seiiku.R
+++ programs/JSPHO-seiiku.R
@@ -17,40 +17,45 @@ Sys.setlocale("LC_TIME", "C") #必須：日本時間にコンピュータ設定�
 
 # csv読み込み
 rawdatapath <- paste0(path , "/rawdata/")
-jspho_csv <- read.csv(paste0(rawdatapath, kJsphoCsv), as.is=T, fileEncoding="CP932", stringsAsFactors=F, header=F)
-colnames(jspho_csv) <- jspho_csv[1, ]
-jspho_csv<- jspho_csv[-1, ]
-jspho_registration_csv <- read.csv(paste0(rawdatapath, kJsphoRegistrationCsv), as.is=T, fileEncoding="CP932", stringsAsFactors=F, header=F)
-colnames(jspho_registration_csv) <- jspho_registration_csv[1, ]
-jspho_registration_csv <- jspho_registration_csv[-1, ]
+jspho_csv <- read.csv(paste0(rawdatapath, kJsphoCsv), as.is=T, fileEncoding="CP932", stringsAsFactors=F)
+jspho_registration_csv <- read.csv(paste0(rawdatapath, kJsphoRegistrationCsv), as.is=T, fileEncoding="CP932", stringsAsFactors=F)
 
-m_jspho_registration_csv <- merge(jspho_registration_csv, jspho_csv, by = "登録コード", all.x = T )
+m_jspho_registration_csv <- merge(jspho_registration_csv, jspho_csv, by  = "登録コード", all.x = T )
 
 # adsの作成
 # 診断年を抽出
 m_jspho_registration_csv$year <- substr(m_jspho_registration_csv$診断年月日, 1, 4)
 dxt_jspho_registration_csv  <- m_jspho_registration_csv[!is.na(m_jspho_registration_csv$year) & m_jspho_registration_csv$year == kYear, ]
 # shimekiri Cut
-jspho_registration_csv1 <-dxt_jspho_registration_csv [format(dxt_jspho_registration_csv$作成日), "%Y%m%d" < kDateCutoff, ] 
-
+jspho_registration_csv1 <- dxt_jspho_registration_csv[format(as.Date(dxt_jspho_registration_csv$作成日), "%Y%m%d") < kDateCutoff, ]
 # age diagnosisを計算
-jspho_registration_csv1$age_diagnosis <- YearDif(jspho_registration_csv1$生年月日, jspho_registration_csv1$診断年月日)
+jspho_registration_csv1$診断時年齢 <- YearDif(jspho_registration_csv1$生年月日, jspho_registration_csv1$診断年月日)
 # Cut registration_csv /age diagnosis is over　20
-dxt_jspho_registration <- jspho_registration_csv1[jspho_registration_csv1$age_diagnosis < 20, ]
+dxt_jspho_registration <- jspho_registration_csv1[jspho_registration_csv1$診断時年齢 < 20, ]
 dxt_jspho_registration$初発時住所_県名  <- ifelse(substr(dxt_jspho_registration$初発時住所, 4, 4) == "県",
                                            substr(dxt_jspho_registration$初発時住所, 1, 4) , substr(dxt_jspho_registration$初発時住所, 1, 3))
 
-#  koko
+#  
 dxt_jspho_registration$住所詳細 <- ifelse(substr(dxt_jspho_registration$初発時住所, 4, 4) == "県", substr(dxt_jspho_registration$初発時住所, 5, 100) ,
                                       substr(dxt_jspho_registration$初発時住所, 4, 100))
-#血液腫瘍性疾患の症例のみ抽出
-df_tumor <- dxt_jspho_registration[dxt_jspho_registration$field176 != "", ]
+#血液腫瘍性疾患の症例とHLHを抽出
+df_tumor <- subset(dxt_jspho_registration, !(is.na(dxt_jspho_registration$field176))) # koko
+df_hlh1 <- subset(dxt_jspho_registration,dxt_jspho_registration$field1 == 21020)
 
-ads <- df_tumor[, c("登録コード", "初発時施設名", "シート作成時団体別施設コード", "作成日", "生死", "最終確認日", "和文名前の一文字目",
-                    "性別", "生年月日", "診断年月日", "初発時住所_県名", "住所詳細", "age_diagnosis", "診断名", "血液腫瘍性 疾患名", 
-                    "ダウン症",　"ダウン症以外の基礎疾患",　"Fanconi貧血",　"Noonan症候群",　"神経線維腫症（NF1）",　"先天性角化不全症",　
-                    "Shwachman-Diamond症候群",　"Diamond-Blackfan貧血",　"特発性再生不良性貧血",　"GATA2異常症",　"RUNX1異常症",
-                    "重症先天性好中球減少症",　"その他の基礎疾患",　"発病形式",　"発病形式が二次性の場合、一次疾患名", "AML FAB分類",
-                    "その他の白血病の疾患分類", "Acute mixed lineage leukemiaの細分類", "CMLの細分類", "NHL Stage(St.Jude)", "NHL 原発部位" ,
-                    "HL Stage(Ann Arbor)", "HL B症状", "HL 原発部位" , "LCH 病期", "LCH 部位（複数選択可）", "先天性免疫不全に随伴するLPD 細分類" )]
+df_tumor_hlh <- rbind(df_tumor, df_hlh1)
+df_tumor_hlh$初発時施設コード3桁 <- NA
 
+ads <- df_tumor_hlh[, c("登録コード", "初発時施設名", "初発時施設コード3桁", "作成日", "生死", "最終確認日", "和文名前の一文字目",
+                    "性別", "生年月日", "診断年月日", "初発時住所_県名", "住所詳細", "診断時年齢", "診断名", "血液腫瘍性.疾患名", 
+                    "ダウン症",　"ダウン症以外の基礎疾患",　"Fanconi貧血",　"Noonan症候群",　"神経線維腫症.NF1.",　"先天性角化不全症",　
+                    "Shwachman.Diamond症候群",　"Diamond.Blackfan貧血",　"特発性再生不良性貧血",　"GATA2異常症",　"RUNX1異常症",
+                    "重症先天性好中球減少症",　"その他の基礎疾患",　"発病形式",　"発病形式が二次性の場合.一次疾患名", "AML.FAB分類",
+                    "その他の白血病の疾患分類", "Acute.mixed.lineage.leukemiaの細分類", "CMLの細分類", "NHL.Stage.St.Jude.", "NHL.原発部位",
+                    "HL.Stage.Ann.Arbor.", "HL.B症状", "HL.原発部位" , "LCH.病期", "LCH.部位.複数選択可.", "先天性免疫不全に随伴するLPD.細分類", "HLH.原発性.続発性")]
+
+
+ads[is.na(ads)] <- ""
+
+# csvの書き出し
+setwd(paste0(path, "/output"))
+write.csv(ads, "seiiiku format.csv", row.names = F)
